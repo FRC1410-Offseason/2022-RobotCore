ee1786ed45ba5aae6f2870bfe3821bd5e47527c9
Rowan Eklund <717538@dpsk12.net>
2022-02-01 14:40:00 -0700

    feat(Intake): Added overloaded constructors for axis control

--- src/main/java/frc/robot/commands/actions/ReverseIntake.java
+++ src/main/java/frc/robot/commands/actions/ReverseIntake.java
@@ -3,20 +3,32 @@ package frc.robot.commands.actions;
 import static frc.robotmap.Constants.*;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.framework.control.input.Axis;
 import frc.robot.subsystems.Intake;
 
 
 public class ReverseIntake extends CommandBase {
     private final Intake intake;
+    private Axis axis = null;
 
     public ReverseIntake(Intake intake) {
         this.intake = intake;
         addRequirements(this.intake);
     }
 
+    public ReverseIntake(Intake intake, Axis axis) {
+        this.intake = intake;
+        this.axis = axis;
+        addRequirements(this.intake);
+    }
+
     @Override
     public void initialize() {
-        this.intake.setSpeed(INTAKE_REVERSE_SPEED);
+        if (this.axis == null) {
+            this.intake.setSpeed(INTAKE_REVERSE_SPEED);
+        } else {
+            this.intake.setSpeed(this.axis.getDeadzoned());
+        }
     }
 
     @Override
