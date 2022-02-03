cd9a454ed67620205e9c727042f430d593e220dc
Rowan Eklund <717538@dpsk12.net>
2022-02-03 12:45:05 -0700

    feat(Network Tables): Added network tables integration
    Cian pls don't kill me for committing directly to main

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,9 +6,13 @@ import frc.robot.commands.actions.SetShooterArmAngle;
 import frc.robot.framework.control.ControlScheme;
 import frc.robot.framework.scheduler.ScheduledRobot;
 import frc.robot.framework.scheduler.TaskScheduler;
-import frc.robot.subsystems.ShooterArm;
+import frc.robot.subsystems.*;
+import static frc.robotmap.IDs.*;
 
 public class Robot extends ScheduledRobot implements ControlScheme {
+	private final String[] autoList = {"Taxi","2Cargo","3CargoTerminal","3CargoUpRight","4Cargo","5Cargo"};
+	private final AnalogInput pressure = new AnalogInput(PRESSURE_SENSOR);
+
 	public static void main(String[] args) {
 		RobotBase.startRobot(Robot::new);
 	}
@@ -32,5 +36,13 @@ public class Robot extends ScheduledRobot implements ControlScheme {
 
 	private Robot() {
 		super(20);
+	}
+
+	@Override
+	public void robotInit() {
+		NetworkTables.networkTables();
+        NetworkTables.setAutoList(autoList);
+		NetworkTables.setCorrectColor(DriverStation.getAlliance().toString());
+		NetworkTables.setPressure(pressure);
 	}
 }
