525bea153c4d705ea1f8cba9e3fc4af662f6eaf3
Cian Capacci <46614295+BeeGuyDude@users.noreply.github.com>
2022-02-04 10:42:09 -0700

    style - removed default comment
    Co-authored-by: Daedalus <16168171+RedDaedalus@users.noreply.github.com>

--- src/main/java/frc/robot/commands/actions/RunIntake.java
+++ src/main/java/frc/robot/commands/actions/RunIntake.java
@@ -41,7 +41,6 @@ public class RunIntake extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        // TODO: Make this return true when this Command no longer needs to run execute()
         return true;
     }
 
