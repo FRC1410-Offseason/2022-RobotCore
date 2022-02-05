9e76e175ad959633f9ebee3f8a040dadb641bf95
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2022-02-05 10:01:38 -0700

    feat: Implement Direction enum

--- src/main/java/frc/robot/framework/control/D.java
+++ src/main/java/frc/robot/framework/control/D.java
@@ -9,7 +9,32 @@ public class D {
 		this.controller = controller;
 	}
 
-	public int getAngle() {
+	public int getRawPOV() {
 		return controller.getPOV();
+	}
+
+	public Direction getDirection() {
+		switch (getRawPOV()) {
+			case 0: return Direction.UP;
+			case 90: return Direction.RIGHT;
+			case 180: return Direction.DOWN;
+			case 270: return Direction.LEFT;
+
+			case -1: default: return Direction.UNKNOWN;
+		}
+	}
+
+	public enum Direction {
+		UNKNOWN(-1),
+		UP(0),
+		RIGHT(90),
+		DOWN(180),
+		LEFT(270);
+
+		final int pov;
+
+		Direction(int pov) {
+			this.pov = pov;
+		}
 	}
 }
