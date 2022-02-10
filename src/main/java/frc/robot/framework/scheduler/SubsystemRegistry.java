8235a256b6c90819cecf7fd0e00b135e4d65e1e1
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2022-02-10 16:52:29 -0700

    feat?(command): Implement command suspension and requirements

--- src/main/java/frc/robot/framework/scheduler/SubsystemRegistry.java
+++ src/main/java/frc/robot/framework/scheduler/SubsystemRegistry.java
@@ -1,13 +1,14 @@
 package frc.robot.framework.scheduler;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SubsystemRegistry {
 
 	private static final Set<Subsystem> subsystems = new HashSet<>();
+	private static final Map<Subsystem, Command> requirementLocks = new HashMap<>();
 
 	public static void register(Subsystem subsystem) {
 		subsystems.add(subsystem);
@@ -15,5 +16,21 @@ public class SubsystemRegistry {
 
 	public static Set<Subsystem> getEntries() {
 		return subsystems;
+	}
+
+	public static void applyLock(Subsystem subsystem, Command command) {
+		requirementLocks.put(subsystem, command);
+	}
+
+	public static boolean ownsLock(Subsystem subsystem, Command command) {
+		return !requirementLocks.containsKey(subsystem) || requirementLocks.get(subsystem) == command;
+	}
+
+	public static Command getLockedCommand(Subsystem subsystem) {
+		return requirementLocks.get(subsystem);
+	}
+
+	public static void releaseLock(Subsystem subsystem, Command command) {
+		requirementLocks.remove(subsystem, command);
 	}
 }
--- src/main/java/frc/robot/framework/scheduler/task/CommandTask.java
+++ src/main/java/frc/robot/framework/scheduler/task/CommandTask.java
@@ -1,6 +1,8 @@
 package frc.robot.framework.scheduler.task;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.framework.scheduler.SubsystemRegistry;
 
 public class CommandTask implements Task {
 
@@ -14,14 +16,28 @@ public class CommandTask implements Task {
 	@Override
 	public void execute() {
 		switch (state) {
+			// The phase responsible for initializing the command and starting its run cycle.
 			case PENDING: {
+				if (!ownsAllLocks()) break;
+				for (Subsystem requirement : command.getRequirements()) {
+					SubsystemRegistry.applyLock(requirement, command);
+				}
+
 				command.initialize();
+
 				state = CommandState.RUNNING;
 				break;
 			}
 
+			// Runs the command, checking subsystem requirements and whether the command is finished.
 			case RUNNING: {
+				if (!ownsAllLocks()) {
+					state = CommandState.SUSPENDED;
+					break;
+				}
+
 				command.execute();
+
 				if (command.isFinished()) {
 					state = CommandState.FINISHED;
 					command.end(false);
@@ -30,12 +46,36 @@ public class CommandTask implements Task {
 				break;
 			}
 
+			// Checks if the command should still be suspended, resuming if necessary.
+			case SUSPENDED: {
+				if (ownsAllLocks()) {
+					state = CommandState.PENDING;
+				}
+
+				break;
+			}
+
+			// Forces the command to finish and ends the Task.
 			case INTERRUPTION_PENDING: {
 				state = CommandState.FINISHED;
 				command.end(false);
 				break;
 			}
+
+			case FINISHED: {
+				for (Subsystem requirement : command.getRequirements()) {
+					SubsystemRegistry.releaseLock(requirement, command);
+				}
+			}
 		}
+	}
+
+	private boolean ownsAllLocks() {
+		for (Subsystem requirement : command.getRequirements()) {
+			if (!SubsystemRegistry.ownsLock(requirement, command)) return false;
+		}
+
+		return true;
 	}
 
 	@Override
@@ -50,6 +90,7 @@ public class CommandTask implements Task {
 	private enum CommandState {
 		PENDING,
 		RUNNING,
+		SUSPENDED,
 		INTERRUPTION_PENDING,
 		FINISHED
 	}
