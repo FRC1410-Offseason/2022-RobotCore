66974f87ec085079172f4586caf4950464a770ae
Santi <39464941+Santi-Z@users.noreply.github.com>
2022-02-15 12:47:12 -0700

    Update src/main/java/frc/robot/util/Trajectories.java
    Co-authored-by: Daedalus <16168171+RedDaedalus@users.noreply.github.com>

--- src/main/java/frc/robot/util/Trajectories.java
+++ src/main/java/frc/robot/util/Trajectories.java
@@ -12,7 +12,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-// import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
