181f2df6eacf9aeccc6f5ac22fcd64940ca71ce7
West <57117414+Westly-Bouchard@users.noreply.github.com>
2022-02-16 13:54:47 -0700

    Style/drivetrain (#45)
    * docs: Added some docs
    
    * docs: Added more docs

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@ import static frc.robotmap.IDs.*;
 import static frc.robotmap.Tuning.*;
 import static frc.robotmap.Constants.*;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
@@ -30,34 +31,63 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 
 public class Drivetrain extends SubsystemBase {
+	/**
+	 * These are used for odometry / pose estimation
+	 * This is not best practice, but it works, and we don't have time to fix it atm
+	 */
 	public double leftEncoderPosition, leftEncoderVelocity, rightEncoderPosition, rightEncoderVelocity;
 
+	/**
+	 * Motors
+	 */
 	public final WPI_TalonFX leftLeader = new WPI_TalonFX(DRIVETRAIN_LEFT_FRONT_MOTOR_ID);
 	public final WPI_TalonFX leftFollower = new WPI_TalonFX(DRIVETRAIN_LEFT_BACK_MOTOR_ID);
 	public final WPI_TalonFX rightLeader = new WPI_TalonFX(DRIVETRAIN_RIGHT_FRONT_MOTOR_ID);
 	public final WPI_TalonFX rightFollower = new WPI_TalonFX(DRIVETRAIN_RIGHT_BACK_MOTOR_ID);
 
+	/**
+	 * Wrapper class for motors that makes them easier to use
+	 */
 	private final DifferentialDrive drive;
 
+	/**
+	 * Odometry but GUD, uses some fancy vector math to accurately update the estimated position of the robot on the field
+	 */
 	public final DifferentialDrivePoseEstimator poseEstimator = new DifferentialDrivePoseEstimator(new Rotation2d(), new Pose2d(),
 			new MatBuilder<>(Nat.N5(), Nat.N1()).fill(STATE_X, STATE_Y, STATE_THETA, STATE_LEFT_DIST, STATE_RIGHT_DIST),
 			new MatBuilder<>(Nat.N3(), Nat.N1()).fill(LOCAL_LEFT_DIST, LOCAL_RIGHT_DIST, LOCAL_THETA),
 			new MatBuilder<>(Nat.N3(), Nat.N1()).fill(VISION_X, VISION_Y, VISION_THETA), 0.005);
 
+	/**
+	 * Used for simulating the drivetrain (duh)
+	 */
 	public DifferentialDrivetrainSim drivetrainSimulator;
 
+	/**
+	 * Used for sim, we send it to networktables to display the robot pose on a field
+	 */
 	public Field2d fieldSim;
 
+	/**
+	 * Only used for simulation, again not best practice, but it works
+	 */
 	public final Encoder rightEncoder = new Encoder(RIGHT_ENCODER_PORTS[0], RIGHT_ENCODER_PORTS[1], false);
 	public final Encoder leftEncoder = new Encoder(LEFT_ENCODER_PORTS[0], LEFT_ENCODER_PORTS[1], false);
 
+	/**
+	 * Simulation stuff, do not do this, it's very bad practice
+	 */
 	public EncoderSim leftEncoderSim;
 	public EncoderSim rightEncoderSim;
 
+	/**
+	 * Gyro and sim handle for the gyro
+	 */
 	public final AHRS gyro = new AHRS(SPI.Port.kMXP);
 	public SimDouble fusedAngle;
 
 	public Drivetrain() {
+		//Config motors
 		initializeTalonFX(leftLeader);
 		initializeTalonFX(leftFollower);
 		initializeTalonFX(rightLeader);
@@ -67,16 +97,28 @@ public class Drivetrain extends SubsystemBase {
 		rightFollower.follow(rightLeader);
 
 		leftLeader.setInverted(true);
+		leftFollower.setInverted(InvertType.FollowMaster);
 
 		drive = new DifferentialDrive(leftLeader, rightLeader);
-		if (RobotBase.isSimulation()) simulationInit();
 
+		//If we are in a simulation, we have to set a couple of things up
+		if (RobotBase.isSimulation()) {
+			simulationInit();
+		}
+
+		//Reset everything to make sure it's good to go
 		resetEncoders();
 		zeroHeading();
-		
+
+		//Send som DATA to networktables
 		NetworkTables.setNavXMagCalibration(gyro.isMagnetometerCalibrated());
 	}
 
+	/**
+	 * Sets up the simulation environment for this subsystem
+	 * Creates the actual simulation instance
+	 * Sets up the encoders for use later
+	 */
 	public void simulationInit() {
 		drivetrainSimulator = new DifferentialDrivetrainSim(
 			DRIVETRAIN_PLANT, DCMotor.getFalcon500(2), GEARING, TRACKWIDTH, WHEEL_DIAMETER, NOISE);
@@ -91,8 +133,18 @@ public class Drivetrain extends SubsystemBase {
 		SmartDashboard.putData("Field", fieldSim);
 	}
 
-	public void periodic() {NetworkTables.setNavXMagDisturbance(gyro.isMagneticDisturbance());}
+	public void periodic() {
+		NetworkTables.setNavXMagDisturbance(gyro.isMagneticDisturbance());
+	}
 
+	/**
+	 * Sets up a falcon:
+	 * - Performs a factory reset
+	 * - Makes sure it's reading from the correct encoder
+	 * - Sets the neutral mode to break
+	 * - And configures a neutral deadband
+	 * @param motor a motor object representing a TalonFX / Falcon 500
+	 */
 	public void initializeTalonFX(WPI_TalonFX motor) {
 		motor.configFactoryDefault();
 		motor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 10);
@@ -100,26 +152,56 @@ public class Drivetrain extends SubsystemBase {
 		motor.configNeutralDeadband(0.001);
 	}
 
+	/**
+	 * Tank drive with deadzoned inputs, expects inputs to be deadzoned before being passed in
+	 * @param deadzonedLeftAxis a double between -1 and 1
+	 * @param deadzonedRightAxis a double between -1 and 1
+	 */
 	public void tankDriveDeadzoned(double deadzonedLeftAxis, double deadzonedRightAxis) {
 		drive.tankDrive(deadzonedLeftAxis, deadzonedRightAxis, false);
 		drive.feed();
 	}
 
+	/**
+	 * Run the drivetrain in arcade mode
+	 * @param forward -1 to 1 representing the desired velocity
+	 * @param rotation -1 to 1 representing the desired angular velocity
+	 */
 	public void arcadeDrive(double forward, double rotation) {
-		drive.arcadeDrive(forward, rotation);}
+		drive.arcadeDrive(forward, rotation);
+	}
 
+	/**
+	 * Not really any different from the `tankDriveDeadzoned()` method
+	 * @param left -1 to 1 representing left velocity
+	 * @param right -1 to 1 representing right velocity
+	 */
 	public void tankDrive(double left, double right) {
 		drive.tankDrive(left, right, false);
 		drive.feed();
 	}
 
-	public Pose2d getPoseEstimation() {return poseEstimator.getEstimatedPosition();}
+	/**
+	 * Get the estimated position of the robot
+	 * @return a Pose2d object representing the position of the robot
+	 */
+	public Pose2d getPoseEstimation() {
+		return poseEstimator.getEstimatedPosition();
+	}
 
+	/**
+	 * Get the current speeds of the wheels
+	 * @return a DifferentialDriveWheelSpeeds object that contains the wheels speeds in m/s
+	 */
 	public DifferentialDriveWheelSpeeds getWheelSpeeds() {
 		if (RobotBase.isSimulation()) {return new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate());}
 		else {return new DifferentialDriveWheelSpeeds(leftEncoderVelocity, rightEncoderVelocity);}
 	}
 
+	/**
+	 * Reset the pose of the robot on the field
+	 * @param pose a starting pose
+	 */
 	public void resetPoseEstimation(Pose2d pose) {
 		if (RobotBase.isSimulation()) {
 			drivetrainSimulator.setPose(pose);
@@ -128,24 +210,47 @@ public class Drivetrain extends SubsystemBase {
 		resetEncoders();
 	}
 
+	/**
+	 * Drive the robot in tank mode using voltage inputs
+	 * @param leftVolts -12 to 12
+	 * @param rightVolts -12 to 12
+	 */
 	public void tankDriveVolts(double leftVolts, double rightVolts) {
 		leftLeader.setVoltage(leftVolts);
 		rightLeader.setVoltage(rightVolts);
 		drive.feed();
 	}
 
+	/**
+	 * Reset the encoders
+	 */
 	public void resetEncoders() {
-		if (RobotBase.isSimulation()) {leftEncoder.reset(); rightEncoder.reset();}
+		if (RobotBase.isSimulation()) {
+			leftEncoder.reset(); rightEncoder.reset();
+		}
 		leftLeader.setSelectedSensorPosition(0);
 		leftFollower.setSelectedSensorPosition(0);
 		rightLeader.setSelectedSensorPosition(0);
 		rightFollower.setSelectedSensorPosition(0);
 	}
 
-	public void setCoast() {leftLeader.setNeutralMode(NeutralMode.Coast); rightLeader.setNeutralMode(NeutralMode.Coast);}
+	/**
+	 * Set the motors to coast mode
+	 */
+	public void setCoast() {
+		leftLeader.setNeutralMode(NeutralMode.Coast); rightLeader.setNeutralMode(NeutralMode.Coast);
+	}
 
-	public void setBrake() {leftLeader.setNeutralMode(NeutralMode.Brake); rightLeader.setNeutralMode(NeutralMode.Brake);}
+	/**
+	 * Set the motors to brake mode
+	 */
+	public void setBrake() {
+		leftLeader.setNeutralMode(NeutralMode.Brake); rightLeader.setNeutralMode(NeutralMode.Brake);
+	}
 
+	/**
+	 * Reset the stored heading of the robot
+	 */
 	public void zeroHeading() {
 		gyro.reset();
 	}
