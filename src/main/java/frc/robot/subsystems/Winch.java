cd52e10c39fd8a3d8752f876b4c2969fbbaec842
Westly Bouchard <westly.bouchard@gmail.com>
2022-03-02 20:08:34 -0700

    chore(Winch): Updates from testing again

--- src/main/java/frc/robot/subsystems/Winch.java
+++ src/main/java/frc/robot/subsystems/Winch.java
@@ -9,9 +9,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.smartdashboard.*;
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robotmap.IDs.*;
