e66774656781c504ba1faf00888ea0b7a1978df5
Westly Bouchard <westly.bouchard@gmail.com>
2022-03-02 20:03:42 -0700

    chore(Storage): Update from testing

--- src/main/java/frc/robot/subsystems/Storage.java
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -5,6 +5,9 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
@@ -15,6 +18,22 @@ import static frc.robotmap.IDs.*;
 import static frc.robotmap.Constants.*;
 
 public class Storage extends SubsystemBase {
+
+	private final NetworkTableInstance instance = NetworkTableInstance.getDefault();
+	private final NetworkTable table = instance.getTable("Storage");
+
+	private final NetworkTableEntry slot1Ball = table.getEntry("Slot 1 Ball");
+	private final NetworkTableEntry slot1Color = table.getEntry("Slot 1 Color");
+
+	private final NetworkTableEntry slot2Ball = table.getEntry("Slot 2 Ball");
+	private final NetworkTableEntry slot2Color = table.getEntry("Slot 2 Color");
+
+	private final NetworkTableEntry lineBreakNt = table.getEntry("Line break");
+	private final NetworkTableEntry color = table.getEntry("Color");
+
+	private final NetworkTableEntry outtakeFlagNt = table.getEntry("Outtake");
+
+	private final NetworkTableEntry encoderNt = table.getEntry("Shooter Arm Encoder");
 
 	/**
 	 * Used for the state objects, correct alliance color is passed into the subsystem on initialization
@@ -59,6 +78,8 @@ public class Storage extends SubsystemBase {
 
 	private boolean intaking = false;
 
+	private boolean manualControl = false;
+
 	/**
 	 * Signals to the rest of the code that there is a cargo of the wrong color somewhere in the storage
 	 */
@@ -84,58 +105,88 @@ public class Storage extends SubsystemBase {
 
 	@Override
 	public void periodic() {
-		// Set lineBreakPrev to the current state of the line break in preparation for the next iteration
-		lineBreakPrev = lineBreak.get();
-	}
+		encoderNt.setDouble(motor.getSelectedSensorPosition());
 
-	public void updateState() {
-		// Falling edge
-		if (lineBreakPrev && lineBreak.get()) {
-			// If a ball is entering the storage, we need to update the state to tell the storage
-//			if (currentState.getSlot1().getBallPresent()) {
-//				currentState.setSlot2(currentState.getSlot1().getColor() == ColorSensorStatus.ALLIANCE);
-//				currentState.resetSlot1();
-//			}
-		// Rising edge
-		} else if (!lineBreakPrev && lineBreak.get()) {
-			// Get the current color from the color sensor
-			var result = colorMatch.matchClosestColor(colorSensor.getColor());
-			if (result.color.equals(RED_TARGET)) {
-				// If the color is red, and we're on the red alliance, we have the right color
-				if (currentAlliance == DriverStation.Alliance.Red) {
-					if (currentState.getSlot2().getBallPresent()) {
+//		if (intaking) {
+//			runStorage(STORAGE_INTAKE_SPEED);
+//		}
+
+		slot1Ball.setBoolean(currentState.getSlot1().getBallPresent());
+		slot2Ball.setBoolean(currentState.getSlot2().getBallPresent());
+
+		lineBreakNt.setBoolean(lineBreak.get());
+		outtakeFlagNt.setBoolean(outtakeFlag);
+
+		var currentColor = colorMatch.matchClosestColor(colorSensor.getColor());
+		if (currentColor.color.equals(RED_TARGET)) {
+			color.setString("RED");
+		} else if (currentColor.color.equals(BLUE_TARGET)) {
+			color.setString("BLUE");
+		} else {
+			color.setString("NONE");
+		}
+
+		if (currentState.getSlot1().getColor() == ColorSensorStatus.ALLIANCE) {
+			slot1Color.setString("ALLIANCE");
+		} else {
+			slot1Color.setString("NOT ALLIANCE");
+		}
+
+		if (currentState.getSlot2().getColor() == ColorSensorStatus.ALLIANCE) {
+			slot2Color.setString("ALLIANCE");
+		} else {
+			slot2Color.setString("NOT ALLIANCE");
+		}
+
+		if (!manualControl) {
+			// Falling edge
+			if (lineBreakPrev && !lineBreak.get()) {
+				motor.set(-1);
+				// If there is a ball in the first position, then move it to the second
+				// and reset the first position to be ready for the next ball
+				if (currentState.getSlot1().getBallPresent()) {
+					currentState.setSlot2(true);
+					currentState.resetSlot1();
+				}
+				// Rising Edge
+			} else if (!lineBreakPrev && lineBreak.get()) {
+				// Stop motor so we can read the color
+				motor.set(0);
+				// Read the color from the color sensor
+				ColorMatchResult result = colorMatch.matchClosestColor(colorSensor.getColor());
+				if (result.color.equals(RED_TARGET)) {
+					if (currentAlliance == DriverStation.Alliance.Red) {
+						// If the detected color is red, and we're on the red alliance, then the color is correct
 						currentState.setSlot1(true);
 					} else {
-						currentState.setSlot2(true);
-					}
-				// If the color is red, and we're on the blue alliance, we have the wrong color
-				} else {
-					if (currentState.getSlot2().getBallPresent()) {
+						// If the detected color is red, and we're on the blue alliance, then the color is incorrect
 						currentState.setSlot1(false);
-					} else {
-						currentState.setSlot2(false);
+						// If the color is wrong then we need to set the outtake flag for the outtake handler
+						outtakeFlag = true;
 					}
-					outtakeFlag = true;
+				} else {
+					if (currentAlliance == DriverStation.Alliance.Red) {
+						// If the detected color is blue, and we're on the red alliance, then the color is incorrect
+						currentState.setSlot1(false);
+						// If the color is wrong then we need to set the outtake flag for the outtake handler
+						outtakeFlag = true;
+					} else {
+						// If the detected color is blue, and we're on the blue alliance, then the color is correct
+						currentState.setSlot1(true);
+					}
 				}
 			} else {
-				// If the color is blue, and we're on the red alliance, we have the wrong color
-				if (currentAlliance == DriverStation.Alliance.Red) {
-					if (currentState.getSlot2().getBallPresent()) {
-						currentState.setSlot1(false);
-					} else {
-						currentState.setSlot2(false);
-					}
-					outtakeFlag = true;
-				// If the color is blue, and we're on the blue alliance, we have the wrong color
+				if (intaking) {
+					motor.set(STORAGE_INTAKE_SPEED);
 				} else {
-					if (currentState.getSlot2().getBallPresent()) {
-						currentState.setSlot1(true);
-					} else {
-						currentState.setSlot2(true);
-					}
+					motor.set(0);
 				}
 			}
+
 		}
+
+		// Set lineBreakPrev to the current state of the line break in preparation for the next iteration
+		lineBreakPrev = lineBreak.get();
 	}
 
 	/**
@@ -152,6 +203,22 @@ public class Storage extends SubsystemBase {
 	 */
 	public void setOuttakeFlag(boolean flag) {
 		outtakeFlag = flag;
+	}
+
+	public boolean isManualControl() {
+		return manualControl;
+	}
+
+	public void setManualControl(boolean manualControl) {
+		this.manualControl = manualControl;
+	}
+
+	public boolean isIntaking() {
+		return intaking;
+	}
+
+	public void setIntaking(boolean intaking) {
+		this.intaking = intaking;
 	}
 
 	public final WPI_TalonSRX getShooterArmMotor() {
@@ -174,12 +241,7 @@ public class Storage extends SubsystemBase {
 		motor.set(power);
 	}
 
-	public boolean isIntaking() {
-		return intaking;
-	}
-
-	public void setIntaking(boolean intaking) {
-		this.intaking = intaking;
+	public void resetOuttakeFlag() {
+		outtakeFlag = false;
 	}
 }
-
