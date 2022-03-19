9249e766e40e0ff24952227b085a8924fafe3561
BeeGuyDude <cian.capacci@gmail.com>
2022-03-19 11:29:31 -0600

    fix - Fixed timer and placement for LowHubShoot Belt Inversion

--- src/main/java/frc/robot/commands/grouped/LowHubShoot.java
+++ src/main/java/frc/robot/commands/grouped/LowHubShoot.java
@@ -2,12 +2,12 @@ package frc.robot.commands.grouped;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.actions.*;
 import frc.robot.subsystems.*;
 
 import static frc.robotmap.Constants.*;
+import static frc.robotmap.Tuning.*;
 
 import java.util.ArrayList;
 
@@ -18,10 +18,8 @@ public class LowHubShoot extends SequentialCommandGroup {
 
 		ArrayList<Command> toRun = new ArrayList<>();
 
-		toRun.add(new ParallelCommandGroup(
-			new RunStorageForTime(storage, STORAGE_REVERSE_TIME, STORAGE_REVERSE_SPEED),
-			new ShooterSpinup(shooter, RPM.getDouble(0))
-		));
+		toRun.add(new RunStorageForTime(storage, STORAGE_REVERSE_TIME, STORAGE_REVERSE_SPEED));
+        toRun.add(new ShooterSpinup(shooter, RPM.getDouble(SHOOTER_LOW_HUB_RPM)));
 		toRun.add(new Shoot(shooter, storage));
 
 		addCommands(toRun.toArray(Command[]::new));
